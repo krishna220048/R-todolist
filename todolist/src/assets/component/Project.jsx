02a1721c120071bcaf033b457f088249b0c85b95
unknown <gg1348260@gamil.com>
2026-01-01 18:45:58 +0530

    Updated CRUD operations

--- todolist/src/assets/component/Project.jsx
+++ todolist/src/assets/component/Project.jsx
@@ -1,53 +1,94 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Project() {
   const [data, setData] = useState("");
   const [data1, setData1] = useState([]);
+  const [editId, setEditId] = useState(null);
+  const [editText, setEditText] = useState("");
 
-  const txt = (e) => {
-    setData(e.target.value);
+  const API = "http://localhost:3000/todolist";
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    const res = await axios.get(API);
+    setData1(res.data);
   };
 
-  const add = () => {
-    if (data) {
-      setData1([...data1, data]);
-      setData("");
-    }
+  const add = async () => {
+    if (!data) return;
+    await axios.post(API, { title: data });
+    setData("");
+    fetchTodos();
   };
-  const complete = (e) => {
-    const textSpan = e.target.parentElement.querySelector("span");
-    if (textSpan) {
-      textSpan.style.color = "red";
-      textSpan.style.textDecoration = "line-through";
-    }
+
+  const del = async (id) => {
+    await axios.delete(`${API}/${id}`);
+    fetchTodos();
   };
+
+  const complete = async (todo) => {
+    await axios.put(`${API}/${todo._id}`, {
+      completed: !todo.completed,
+    });
+    fetchTodos();
+  };
+
+  const startEdit = (todo) => {
+    setEditId(todo._id);
+    setEditText(todo.title);
+  };
+
+  const update = async () => {
+    await axios.put(`${API}/${editId}`, {
+      title: editText,
+    });
+    setEditId(null);
+    setEditText("");
+    fetchTodos();
+  };
+
   return (
     <>
       <h1 style={{ color: "white" }}>TO DO LIST</h1>
+
       <div className="main">
         <div className="main1">
           <input
             type="text"
             placeholder="Enter the text"
             value={data}
-            onChange={txt}
+            onChange={(e) => setData(e.target.value)}
           />
           <button onClick={add}>Add</button>
         </div>
+
         <div className="content">
           <ol>
-            {data1.map((val, index) => (
-              <li key={index}>
-                <span>{val}</span>
-                <button
-                  key={index}
-                  onClick={(e) => {
-                    e.target.parentElement.remove();
-                  }}
-                >
-                  Delete
-                </button>
-                <button onClick={complete}>Complete</button>
+            {data1.map((todo) => (
+              <li key={todo._id}>
+                {editId === todo._id ? (
+                  <>
+                    <input
+                      className="edit"
+                      value={editText}
+                      onChange={(e) => setEditText(e.target.value)}
+                    />
+                    <button onClick={update}>Update</button>
+                    <button onClick={() => setEditId(null)}>Cancel</button>
+                  </>
+                ) : (
+                  <>
+                    <span>{todo.title}</span>
+
+                    <button onClick={() => del(todo._id)}>Delete</button>
+
+                    <button onClick={() => startEdit(todo)}>Edit</button>
+                  </>
+                )}
               </li>
             ))}
           </ol>
